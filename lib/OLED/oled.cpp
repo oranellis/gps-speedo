920f93656f90cdda539bc3af023c91a533b26fa1
Karl Hartmann <2566374h@student.gla.ac.uk>
2022-12-19 19:14:05 +0000

    fixed time not displaying

--- lib/OLED/oled.cpp
+++ lib/OLED/oled.cpp
@@ -25,7 +25,7 @@ void Display::UpdateDisp(float speed_inp, int sats, float timer) {
     u8g2->printf("%.1f\n", speed_inp);
     u8g2->setFont(u8g2_font_profont17_mr);
     u8g2->setCursor(0, 64);
-    u8g2->print(0.00);
+    u8g2->printf("%.1f\n", timer);
     u8g2->setCursor(42, 64);
     u8g2->printf("*%i", sats);
     u8g2->drawStr(84, 64, "km/h");
