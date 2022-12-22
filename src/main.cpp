04fedcb9ee673c99b9d82fd38ce51a2ec2c46864
Karl Hartmann <2566374h@student.gla.ac.uk>
2022-12-22 11:37:24 +0000

    begun changing to state based design

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,10 @@
 Display display;
 NAV_PVT pvt;
 
-uint8 error = 0;
+enum states{Accel, Menu, Error};
+enum states state = Accel;
+
+short error = 0;
 String error_msg;
 
 double prev_speed = 0;
@@ -47,7 +50,7 @@ void setup() {
 
     while (!ProcessGPS(&pvt)) {
         if (millis() - start_time > 5000) {
-            error = 1;
+            state = Error;
             error_msg = "No GPS Data";
             break;
         }
@@ -56,63 +59,71 @@ void setup() {
 }
 
 void loop() {
-    if (millis() >= debounce_tmr && debounce_tmr != 0) {
-        debounce_tmr = 0;
-        if (!digitalRead(12)) {
-            if (menu) {
-                menu = 0;
-            } else {
-                menu = 1;
+    switch (state) {
+        case Accel :
+            if (millis() >= debounce_tmr && debounce_tmr != 0) {
+                debounce_tmr = 0;
+                if (!digitalRead(12)) {
+                    if (menu) {
+                        menu = 0;
+                    } else {
+                        menu = 1;
+                    }
+                }
             }
-        }
-    }
 
-    /*while (menu) {
-        if (!menu) {
-            menu = 0;
-        }
-    }*/
+            /*while (menu) {
+                if (!menu) {
+                    menu = 0;
+                }
+            }*/
 
-    if (ProcessGPS(&pvt)) {
-        error = 0;
-        cur_time = millis();
-        gps_hz = (cur_time - last_gps_refresh) / 1000;
-        last_gps_refresh = cur_time;
+            if (ProcessGPS(&pvt)) {
+                error = 0;
+                cur_time = millis();
+                gps_hz = (cur_time - last_gps_refresh) / 1000;
+                last_gps_refresh = cur_time;
 
-        prev_speed = ground_speed;
-        ground_speed = pvt.gnd_speed;
-        interp_count = 0;
-        last_disp_refresh = cur_time;
+                prev_speed = ground_speed;
+                ground_speed = pvt.gnd_speed;
+                interp_count = 0;
+                last_disp_refresh = cur_time;
 
-        if (acc_started) {
-            timer = millis() - acc_start_time;
-            if ((int)(ground_speed * 0.0036) > end_speed) {
-                acc_ready = 0;
-                acc_started = 0;
-            } else if ((int)(ground_speed * 0.0036) < start_speed) {
-                timer = 0;
-                acc_ready = 1;
-                acc_started = 0;
+                /*if (acc_started) {
+                    timer = millis() - acc_start_time;
+                    if ((int)(ground_speed * 0.0036) > end_speed) {
+                        acc_ready = 0;
+                        acc_started = 0;
+                    } else if ((int)(ground_speed * 0.0036) < start_speed) {
+                        timer = 0;
+                        acc_ready = 1;
+                        acc_started = 0;
+                    }
+                } else if ((acc_ready) && ((int)(ground_speed * 0.0036) > start_speed)) {
+                    acc_start_time = millis();
+                    timer = 0;
+                    acc_started = 1;
+                    acc_ready = 0;
+                } else if ((!acc_ready) && ((int)(ground_speed * 0.0036) < start_speed)) {
+                    timer = 0;
+                    acc_ready = 1;
+                } */
+
+            } else if ((millis() - last_gps_refresh) > 1000)  {
+                error = 1;
+                error_msg = "No GPS Data";
             }
-        } else if ((acc_ready) && ((int)(ground_speed * 0.0036) > start_speed)) {
-            acc_start_time = millis();
-            timer = 0;
-            acc_started = 1;
-            acc_ready = 0;
-        } else if ((!acc_ready) && ((int)(ground_speed * 0.0036) < start_speed)) {
-            timer = 0;
-            acc_ready = 1;
-        }
 
-    } else if ((millis() - last_gps_refresh) > 1000)  {
-        error = 1;
-        error_msg = "No GPS Data";
-    }
-
-    if (((millis() - last_disp_refresh) >= (1000 / DISPLAY_HZ)) && (error == 0)) {
-        print_val = Interp(prev_speed, ground_speed, interp_count, INTERP_NUM) * 0.0036;
-        display.UpdateDisp(print_val, pvt.num_sv, ((float)timer / 1000));
-    } else if (error) {
-        display.ErrorMsg(error_msg.c_str());
+            if (((millis() - last_disp_refresh) >= (1000 / DISPLAY_HZ)) && (error == 0)) {
+                print_val = Interp(prev_speed, ground_speed, interp_count, INTERP_NUM) * 0.0036;
+                display.UpdateDisp(print_val, pvt.num_sv, ((float)timer / 1000));
+            } else if (error == 1) {
+                display.ErrorMsg(error_msg.c_str());
+            }
+            break;
+        default :
+            while (1) {
+                ;;
+            }
     }
 }
